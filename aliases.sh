9156f57cb6079251b0b4b2820f81f41288b1e203
biboletin <shady_fan@abv.bg>
2025-10-10 18:38:09 +0300

    Add some useful aliases

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 IFS=$'\n\t'
+
+alias home="cd ~/"
+alias downloads="cd ~/Downloads/"
+alias documents="cd ~/Documents/"
+alias desktop="cd ~/Desktop/"
+alias www="cd /var/www/html/"
+alias c="clear"
+alias reload="sudo systemctl restart apache2.service mysql phpsessionclean.service"
